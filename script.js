7825cc03412be50df8a76ed3e1c83adbb4806f96
Sachin Kumar <kumarsachinkain01>
2024-04-18 15:38:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+console.log("infine scroll")
+const imageContainer = document.getElementById('img-container');
+const loader = document.getElementById('loader');
+
+let photosArray = []
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+
+
+let count = 10
+const apikey = "_DDIVJSgdK-GI1wA3aHOtxC9YTt8tCY6-4jMk7guznY";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apikey}&count=${count}`;
+
+function setAttribute(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+
+
+function imageLoaded() {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        console.log("ready", ready);
+    }
+}
+
+
+// get picture from APi and show in DoM
+function displayPhotos() {
+    totalImages = photosArray.length;
+
+    imagesLoaded = 0;
+    // for the next 10 images 
+
+    photosArray.forEach((photo) => {
+        const item = document.createElement("a");
+        
+        setAttribute(item, {
+            href: photo.links.html,
+            target: "_blank",
+        });
+
+        const img = document.createElement("img");
+        setAttribute(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
+        img.addEventListener('load', imageLoaded);
+
+        item.append(img);
+
+        imageContainer.append(item);
+    });
+}
+
+// get  photos from the Api 
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// check to see if scrolling is coming near to bottom of the page;
+window.addEventListener("scroll", () =>{
+    if(window.scrollY + window.innerHeight >= document.body.offsetHeight && ready){
+        ready = false;
+        getPhotos();
+    }
+});
+
+getPhotos();
